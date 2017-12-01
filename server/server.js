046301cfbdbc3a109bc97bc82e8dc93392a7db21
Modesto Tamayo <modesto.t.tamayo@gmail.com>
2017-12-01 14:52:44 -0600

    Added new message and create message events

--- server/server.js
+++ server/server.js
@@ -14,9 +14,19 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', {
+    from: 'joe',
+    text: 'nice chat app',
+    createdAt: 123
+  });
+
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+  });
+
   socket.on('disconnect', () => {
     console.log("User disconected");
-  })
+  });
 });
 
 server.listen(port, () => {
